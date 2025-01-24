27019b33507ecf8215a907a22c0f85bf8a431cf5
c03zi <cozl.il@yandex.ru>
2025-01-24 06:52:23 -0500

    update print forms and add finish-function

--- TI_ON_Python.py
+++ TI_ON_Python.py
@@ -16,63 +16,51 @@ load_dotenv()
 
 # Получения нужного API ключа
 
-def get_api_key(api_key, ti_systems):
-    api_key = os.getenv('API_KEY_'+ti_systems)
+def get_api_key(ti_systems):
+    api_key = os.getenv('API_KEY_' + ti_systems)
 
     if not api_key:
         print("Введи API ключ для ", ti_systems)
         api_key = input()
         with open('.env', 'a') as env_file:
-            name_api_env = 'API_KEY_'+ti_systems
+            name_api_env = 'API_KEY_' + ti_systems
             env_file.write(f'{name_api_env}={api_key}\n')
         
-        print("API ключ сохранен в переменной окружения.")
+        print("API ключ сохранен в переменной окружения\n")
     else:
-        print("API ключ загружен из переменной окружения.")
+        print("API ключ загружен из переменной окружения\n")
     
     return api_key
 
-print("API ключ для VirusTotal")
+print("\nAPI ключ для VirusTotal\n")
 ti_vt = 'VirusTotal'
-api_vt = 0
-api_vt = get_api_key(api_vt, ti_vt)
+api_vt = get_api_key(ti_vt)
 
-print("API ключ для KTI")
+print("API ключ для KTI\n")
 ti_ktip = 'KTIP'
-api_ktip = 0
-api_ktip = get_api_key(api_ktip, ti_ktip)
+api_ktip = get_api_key(ti_ktip)
 
 # WHOIS - information about IPv4 address
 
 def whois(add):
-    print("WHOIS")
+    print("WHOIS\n")
 
     lnk = "http://ip-api.com/json/"+add
     response = requests.get(lnk)
     p = response.text
     text = json.loads(p)
-
-    country_code = text['countryCode']
-    print("Код страны: ", country_code)
-
-    country = text['country']
-    print("Страна: ", country)
+    print(f"Код страны: {text['countryCode']}\n"
+          f"Страна: {text['country']}\n"
+          f"Город: {text['city']}\n"
+          f"Координаты расположения:\n\t lat: {text['lat']}\n\t lon: {text['lon']}\n"
+          f"Провайдер: {text['as']}\n")
     
-    city = text['city']
-    print("Город: ", city)
-    
-    coordinate1 = text['lat']
-    coordinate2 = text['lon']
-    print("Координаты расположения: \n lat: ", coordinate1, "\n lon: ", coordinate2)
-    
-    provider = text['as']
-    print("Провайдер: ", provider, "\n")
 
 # Формирование GET запроса для получения ответа о запрашиваемом адресе
 
 def api_get(add, url, api):
 
-    api_url = url+add
+    api_url = url + add
 
     if api == api_vt:
         headers_api = {"x-apikey": api}
@@ -87,8 +75,7 @@ def api_get(add, url, api):
 # Форматирование json
 
 def format_json(stats):
-    result_str = json.dumps(stats, indent=4, ensure_ascii=False)
-    return result_str
+    return json.dumps(stats, indent=4, ensure_ascii=False)
 
 
 # VirusTotal - предоставление информации об IP адресе 
@@ -103,14 +90,13 @@ def virustotal(add, api):
     else:
         print("Репутация адреса: ненадежный!") 
     
-    print("Статистика адреса: ")
-
-    stats = api_get(add, url, api)['data']['attributes']['last_analysis_stats']
-    print(format_json(stats))
-    
+    stats = api_get(add, url, api)['data']['attributes']['last_analysis_stats']    
     my_time = api_get(add, url, api)['data']['attributes']['last_modification_date']
-    print("Дата последнего анализа: ", time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(my_time)))
-    print('\n')
+
+    print(f"Статистика адреса: \n{format_json(stats)}\n"
+          f"Дата последнего анализа: {time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(my_time))}\n")
+    
+    return rep
     
 #Kaspersky Threat Intelligence Portal - предоставление информации об IP адресе 
 
@@ -119,16 +105,32 @@ def ktip(add, api):
     url = 'https://opentip.kaspersky.com/api/v1/search/ip?request='
     
     rep = api_get(add, url, api)["Zone"]
-    print("Уровень опасности: ", rep)
-    if rep != 'Green' or rep != 'Grey':
-       print("Вывод: адрес ненадежный!")
+    print("Уровень опасности: \n", rep)
+    if rep != 'Green' and rep != 'Grey':
        categories = api_get(add, url, api)["IpGeneralInfo"]["CategoriesWithZone"]
-       print("Категория опасности: ", format_json(categories)) 
+       print(f"Вывод: адрес ненадежный!\n"
+             f"Категория опасности: {format_json(categories)}\n") 
+       if rep == 'Orange':
+           dang = 2
+       elif rep == 'Red':
+           dang = 3
+    elif rep == 'Grey':
+        print("Адрес не известен\n")
+        dang = 1
     else:
-        print("Вывод: адрес надежный")
+        print("Вывод: адрес надежный\n")
+        dang = 0
+
+    return dang
 
 
 
 whois(ip)
-virustotal(ip, api_vt)
-ktip(ip, api_ktip)
+vt = virustotal(ip, api_vt)
+kasp = ktip(ip, api_ktip)
+if vt == kasp and vt == 0:
+    print(f"Итоговый вывод по адресу {ip}: надежный!")
+elif vt != kasp and (vt == 0 or kasp == 0):
+    print(f"Итоговый вывод по адресу {ip}: точно определить затруднительно, требуется дополнительный анализ")
+else:
+    print(f"Итоговый вывод по адресу {ip}: ненадежный!") 
